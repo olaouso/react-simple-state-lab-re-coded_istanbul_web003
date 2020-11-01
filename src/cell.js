5b51b874ac5627ce06cbabcc393f7b883d4eeead
Ola Ouso <ouso.ola@gmail.com>
2020-11-01 20:14:49 +0000

    Automatically backed up by Learn

--- src/cell.js
+++ src/cell.js
@@ -1,20 +1,28 @@
-import React from 'react'
+import React, { Component } from 'react';
 
-export default class Cell extends React.Component {
-constructor(props) {
+export default class Cell extends Component {
+  
+  constructor(props) {
     super()
-    this.state = {color : props.value} // ...define initial state with a key of 'color' set to the 'value' prop
+    this.state = {
+      color: props.value
+    }
   }
-
+  
   handleClick = () => {
-      this.setState({
-          color:'#333'
-      })
+    this.setState({
+      color: '#333'
+    })
   }
-
+  
   render() {
-      return (
-          <div className="cell" style={{backgroundColor: this.state.color}} onClick={this.handleClick}></div>
-      )
+    return (
+      <div 
+        className="cell"
+        style={{backgroundColor: this.state.color}}
+        onClick={this.handleClick}>
+      </div>
+    )
   }
-} 
+  
+}
